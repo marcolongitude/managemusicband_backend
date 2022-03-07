b1b72bb6c97e3a3ffd73bf08b04f3e319db81815
MarcoAurelio <marcocpdti@gmail.com>
2022-03-06 22:32:50 -0300

    corrections

--- src/modules/session/controllers/index.ts
+++ src/modules/session/controllers/index.ts
@@ -1,16 +1,13 @@
 import { Request, Response } from "express";
-import { IUser } from "interfaces";
 import * as yup from "yup";
 
 import { CustomError } from "../../../appError/custom-error.model";
-import checkPassword from "../../../util/checkPassword";
-import { createToken } from "../../../util/createToken";
 import { setSessionApp } from "../services";
 
 export const SessionController = async (
     request: Request,
     response: Response
-) => {
+): Promise<Response> => {
     const { email, password } = request.body;
 
     const schema = yup.object().shape({
@@ -24,25 +21,7 @@ export const SessionController = async (
         });
     }
 
-    const user: IUser = await setSessionApp({ email, password });
-
-    if (!user) {
-        throw new CustomError("User not found", 404);
-    }
-
-    const { name_users, email_users, permission, password_hash } = user;
-
-    console.log("*****************************");
-    console.log(checkPassword(password, password_hash));
-    console.log("*****************************");
-
-    if (!(await checkPassword(password, password_hash))) {
-        throw new CustomError("Password does not match", 401, {
-            error: "password does not match",
-        });
-    }
-
-    const token = createToken({ name_users, email_users, permission });
+    const token: string = await setSessionApp({ email, password });
 
     return response.status(200).json({ token });
 };
--- src/modules/users/controllers/index.ts
+++ src/modules/users/controllers/index.ts
@@ -12,14 +12,20 @@ import {
     createUserData,
 } from "../services";
 
-export const getUsers = async (request: Request, response: Response) => {
+export const getUsers = async (
+    request: Request,
+    response: Response
+): Promise<Response> => {
     const result = await getUsersData();
 
     if (result) return response.status(200).json({ data: result });
     throw new CustomError("Users not found", 404);
 };
 
-export const getUserById = async (request: Request, response: Response) => {
+export const getUserById = async (
+    request: Request,
+    response: Response
+): Promise<Response> => {
     const { id_users } = request.params;
 
     const result = await getUserByIdData({ id_users });
@@ -28,7 +34,10 @@ export const getUserById = async (request: Request, response: Response) => {
     throw new CustomError("Users not found", 404);
 };
 
-export const getUserEmail = async (request: Request, response: Response) => {
+export const getUserEmail = async (
+    request: Request,
+    response: Response
+): Promise<Response> => {
     const { email_users } = request.params;
 
     const result = await getUserByEmailData({ email_users });
@@ -37,7 +46,10 @@ export const getUserEmail = async (request: Request, response: Response) => {
     throw new CustomError("Users not found", 404);
 };
 
-export const updateUserId = async (request: Request, response: Response) => {
+export const updateUserId = async (
+    request: Request,
+    response: Response
+): Promise<Response> => {
     const { id_users, name_users } = request.body;
     const schema = yup.object().shape({
         id_users: yup.string().required(),
@@ -61,7 +73,10 @@ export const updateUserId = async (request: Request, response: Response) => {
     throw new CustomError("Users not found", 404);
 };
 
-export const createUser = async (request: Request, response: Response) => {
+export const createUser = async (
+    request: Request,
+    response: Response
+): Promise<Response> => {
     const { name_users, email_users, permission, password } = request.body;
     const schema = yup.object().shape({
         name_users: yup.string().required(),
@@ -95,7 +110,10 @@ export const createUser = async (request: Request, response: Response) => {
     return response.status(200).send();
 };
 
-export const deleteUserId = async (request: Request, response: Response) => {
+export const deleteUserId = async (
+    request: Request,
+    response: Response
+): Promise<Response> => {
     const { id_users } = request.params;
 
     await deleteUserById({ id_users });
